72f1b4e6a824073add5da1abcd60bc9ba9f6fffa
Alex Donnelly <a.james.donnelly@gmail.com>
2020-09-01 12:55:53 -0500

    user data complete

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
-// creating a route that the front-end can request data from
-const { animals } = require('./data/animals');
+
+// We'll have to not only use .push() to save the new data 
+//in this local server.js copy of our animal data, but 
+//we'll also have to import and use the fs library to write 
+//that data to animals.json.
+const fs = require('fs');
+const path = require('path');
+
 
 //require express to bring into server.js
 const express = require('express');
+
+// creating a route that the front-end can request data from
+// whenever we use require() to import data or functionality, it's only reading the data and creating a copy of it to use
+const { animals } = require('./data/animals');
+
 
 // When Heroku runs our app, it sets an environment variable called process.env.PORT. We're going to tell our app to use that port, if it has been set, and if not, default to port 80
 const PORT = process.env.PORT || 3001;
@@ -12,8 +23,13 @@ so that we can later chain on methods to the Express.js server.*/
 const app = express();
 
 // parse incoming string or array data
+// middleware-This is a method executed by our Express.js server that mounts a function to the server that our requests will pass through before getting to the intended endpoint.
+// method built into Express.js. It takes incoming POST data and converts it to key/value pairings that can be accessed in the req.body object.
+//The extended: true option set inside the method call informs our server that there may be sub-array data nested in it as well, so it needs to look as deep into the POST data as possible to parse all of the data correctly.
 app.use(express.urlencoded({ extended: true }));
+
 // parse incoming JSON data
+//The express.json() method we used takes incoming POST data in the form of JSON and parses it into the req.body JavaScript object. 
 app.use(express.json());
 
 // app.listen(3001, () => {
@@ -67,10 +83,36 @@ app.listen(PORT, () => {
     // return the filtered results:
     return filteredResults;
   }
+
+
 //   function that takes in the id and array of animals and returns a single animal object
   function findById(id, animalsArray) {
     const result = animalsArray.filter(animal => animal.id === id)[0];
     return result;
+  }
+
+  //function that accepts the POST route's req.body value and the array we want to add the data to
+  //using the fs.writeFileSync() method, which is the synchronous version of fs.writeFile() and doesn't 
+  //require a callback function.
+  //We want to write to our animals.json file in the data subdirectory, 
+  //so we use the method path.join() to join the value of __dirname, 
+  //which represents the directory of the file we execute the code in, 
+  //with the path to the animals.json file.
+  //Next, we need to save the JavaScript array data as JSON, so we use 
+  //JSON.stringify() to convert it.
+  //The other two arguments used in the method, null and 2, are means 
+  //of keeping our data formatted. The null argument means we don't 
+  //want to edit any of our existing data; if we did, we could pass 
+  //something in there. The 2 indicates we want to create white space 
+  //between our values to make it more readable.
+  function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+      path.join(__dirname, './data/animals.json'),
+      JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+    return animal;
   }
 
 //get callback
@@ -89,6 +131,8 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
   });
 
+  // new GET route for animals-we want to use params, which needs to be defined in a new route pather, so we need to make one
+  //param route must come after get route
   app.get('/api/animals/:id', (req, res) => {
     const result = findById(req.params.id, animals);
     if (result) {
@@ -100,8 +144,44 @@ app.get('/api/animals', (req, res) => {
     }
   });
 
-  app.post('/api/animals', (req, res) => {});app.post('/api/animals', (req, res) => {
-    // req.body is where our incoming content will be
-    console.log(req.body);
-    res.json(req.body);
-  });
+  //route that listens for POST requests, not GET requests
+  //method of the app object that allows us to create routes
+  app.post('/api/animals', (req, res) => {
+    // set id based on what the next index of the array will be
+    //when we receive new post data to be added to the animals.json 
+    //file, we'll take the length property of the animals array 
+    //(because it's a one-to-one representation of our animals.json 
+    //file data) and set that as the id for the new data.
+    req.body.id = animals.length.toString();
+  
+    // add animal to json file and animals array in this function
+    // if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+  }
+});
+
+  function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+      return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+      return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+      return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+      return false;
+    }
+    return true;
+  }
+
+  // module.exports = {
+  //   filterByQuery,
+  //   findById,
+  // };
+  
